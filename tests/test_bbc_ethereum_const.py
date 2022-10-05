c6cf58879457b5a9d2ced5d1903af910d3230c1c
ks91 <ks91@sfc.wide.ad.jp>
2022-10-05 14:02:29 +0900

    a correction in the process of supporting goerli Ethereum testnet.

--- tests/test_bbc_ethereum_const.py
+++ tests/test_bbc_ethereum_const.py
@@ -3,7 +3,7 @@
 # Those who wants to run test_bbc_ethereum[1-2].py on goerli test network
 # needs to define the following, and to load the account with an appropriate
 # amount of goerli ETH.
-TEST_PRIVATE_KEY = '0xe65b5fda980d5deb87a2655c66d78bae5b212658946103882e806ee3234ada31'
-TEST_INFURA_PROJECT_ID = '64ac8922d8d6432d8bbaa3af8a98d1f1'
+TEST_PRIVATE_KEY = ''
+TEST_INFURA_PROJECT_ID = ''
 
 # end of test_bbc_ethereum_const.py
